3b1b620a91e96b4e9ab6112e4e773b753e74af16
Thanisha23 <thanishab77@gmail.com>
2023-04-30 18:13:49 +0530

    Added FAQs Component

--- src/components/FAQs.jsx
+++ src/components/FAQs.jsx
@@ -7,8 +7,12 @@ const FAQs = () => {
   return (
     <section className="faqs">
       <div className="container faqs__container">
-        <SectionalHead icon={FaQuestion} title="FAQs" />
-        <div className="faqs__wrapper">{}</div>
+        <SectionalHead icon={<FaQuestion />} title="FAQs" />
+        <div className="faqs__wrapper">
+          {faqs.map(({ id, question, answer }) => {
+            return <FAQ key={id} question={question} answer={answer} />;
+          })}
+        </div>
       </div>
     </section>
   );
